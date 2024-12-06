0e34f11b51931ad26678ee530f540410886f691e
LatiVectors <lateefolayinka97@gmail.com>
2024-12-06 19:56:15 +0100

    Delete js directory

--- js/Script.js
+++
@@ -1,9 +0,0 @@
-﻿function change() {
-    document.getElementById('dem').innerHTML = "Every department inside the organization, including the lab, factory floor, farm home, workshop, and water treatment building, was introduced to me."
-}
-let Name = "Abdullateef website.";
-const Cars = ["Venza", "Corolla", " Camry", " Highlander car"];
-
-function getCars() {
-    document.getElementById('demo').innerHTML = Cars;
-}
