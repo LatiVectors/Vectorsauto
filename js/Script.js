d08ff15e9622cd9ac1ccf82ba13d9e0be3a41a69
LatiVectors <lateefolayinka97@gmail.com>
2024-12-06 04:53:32 +0100

    Initial upload of web pages

---
+++ js/Script.js
@@ -0,0 +1,9 @@
+﻿function change() {
+    document.getElementById('dem').innerHTML = "Every department inside the organization, including the lab, factory floor, farm home, workshop, and water treatment building, was introduced to me."
+}
+let Name = "Abdullateef website.";
+const Cars = ["Venza", "Corolla", "Camry", "Highlander"];
+
+function getCars() {
+    document.getElementById('demo').innerHTML = Cars;
+}
