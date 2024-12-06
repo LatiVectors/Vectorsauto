6b02a8a43ebe39b2df4f8e024229a4969c4e4e75
LatiVectors <lateefolayinka97@gmail.com>
2024-12-06 09:20:38 +0100

    Update Script.js

--- js/Script.js
+++ js/Script.js
@@ -2,7 +2,7 @@
     document.getElementById('dem').innerHTML = "Every department inside the organization, including the lab, factory floor, farm home, workshop, and water treatment building, was introduced to me."
 }
 let Name = "Abdullateef website.";
-const Cars = ["Venza", "Corolla", "Camry", "Highlander"];
+const Cars = ["Venza", "Corolla", " Camry", " Highlander car"];
 
 function getCars() {
     document.getElementById('demo').innerHTML = Cars;
